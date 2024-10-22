b42b461cc526074d4df4c36accdcfb930ce70fdd
Florian Weigelt <florian@weige.lt>
2024-10-22 10:26:47 -0500

    added comments

--- Part2/main.py
+++ Part2/main.py
@@ -2,34 +2,32 @@ from utils import URL_Ebay, load_items, scrape_reviews, save_data
 import os
 
 
-def main(input_file, output_file, max_review_pages=None, verbose=False):
+def main(input_file: str, output_file: str, max_review_pages:int=None, verbose:bool=False):
     
+    items = load_items(os.path.abspath(input_file))                     # load the items and links from input file
+    if verbose: print(f"Reading list... {len(items)} items found.")     # print number of items to scrape
 
+    if not max_review_pages:                                            # handle not defined max pages
+        max_review_pages = float("inf")                                 # none = inf
 
-    items = load_items(os.path.abspath(input_file))
-    if verbose: print(f"Reading list... {len(items)} items found.") 
-
-    if not max_review_pages:
-        max_review_pages = float("inf")
-
-    for item in items:
+    for item in items:                                                  # iterate through every item in the list
         if verbose: print(f"Scraping Item: {item['name']}")
-        p = 1
+        p = 1                                                           # set starting page number to 1
 
-        url_handler = URL_Ebay(item["link"])
-        reviews_total = []
-        while p <= max_review_pages:
+        url_handler = URL_Ebay(item["link"])                            # initialize an url handler with the product link
+        reviews_total = []                                              # initialize list of reviews
+        while p <= max_review_pages:                                    # go through all review pages
             try:
-                reviews = scrape_reviews(url_handler.get_review_page(p))
-                if verbose: print(f"  Page {p}: {len(reviews)} reviews found")
-            except IndexError:
-                break
-            reviews_total.extend(reviews)
-            p += 1
-        item["reviews"] = reviews_total
+                reviews = scrape_reviews(url_handler.get_review_page(p))    # scrape reviews
+                if verbose: print(f"  Page {p}: {len(reviews)} reviews found")  # print number of reviews found on page
+            except IndexError:                                          # if there are no reviews on the page
+                break                                                   # leave the loop 
+            reviews_total.extend(reviews)                               # append page reviews to total review list
+            p += 1                                                      # go to the next page
+        item["reviews"] = reviews_total                                 # add reviews to item dict
         if verbose: print()
     
-    save_data(items, output_file)
+    save_data(items, output_file)                                       # save updated item dict as json to file
 
 
 if __name__ == "__main__":
--- Part2/utils.py
+++ Part2/utils.py
@@ -6,6 +6,9 @@ import requests
 
 
 class URL_Ebay:
+    """
+    This class handles eBay links and can return specific pages of the review page.
+    """
     def __init__(self, url):
         self.url = url
         self.product_name = None
@@ -16,21 +19,24 @@ class URL_Ebay:
         # URL Format:
         # https://www.ebay.com/urw/<product_name>/product-reviews/<offer_code>?pgn=<page>
         pattern = r"https://www\.ebay\.com/urw/(?P<product_name>[^/]+)/product-reviews/(?P<offer_code>[^/]+)"
-        match = re.match(pattern, self.url)
+        match = re.match(pattern, self.url)     # use regex to find variable parts of the url
         if match:
             self.product_name = match.group('product_name')
             self.offer_code = match.group('offer_code')
     
     def get_review_page(self, page_number:int):
+        # return the link to the review page at a speciefied page number
         return f"https://www.ebay.com/urw/{self.product_name}/product-reviews/{self.offer_code}?pgn={page_number}"
 
 
 def load_items(file_path:str):
+    # loads item names and links from json file 
     with open(file_path, 'r') as file:
         data = json.load(file)
         return data["items"]    
     
 def save_data(data, file_path):
+    # saves item names, links and reviews to json file
     try:
         with open(file_path, 'w') as json_file:
             json.dump(data, json_file, indent=4)
@@ -38,42 +44,46 @@ def save_data(data, file_path):
         print(f"An error occurred while writing to the file: {e}")
 
 
-def scrape_reviews(url):
+def scrape_reviews(url: str):
+    """
+    This function takes the URL of a eBay review Page and 
+    scrapes the title and text for each review on the page.
+    """
     try:
-        ua = UserAgent()
+        ua = UserAgent()                            # user agent object
         headers = {
-            'User-Agent': ua.random,
+            'User-Agent': ua.random,                # generate random useragent to cofuse bot protection systems
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
-            'Accept-Language': 'en-US,en;q=0.9',
-            'Accept-Encoding': 'gzip, deflate, br',
-            'Connection': 'keep-alive',
-            'Referer': 'https://www.ebay.com/',
-            'Upgrade-Insecure-Requests': '1',
-            'DNT': '1',
+            'Accept-Language': 'en-US,en;q=0.9',    # also use as many of these headers to seem like a 
+            'Accept-Encoding': 'gzip, deflate, br', # real browser instead of a bot.
+            'Connection': 'keep-alive',             
+            'Referer': 'https://www.ebay.com/',     # Doing scraping without all these headers
+            'Upgrade-Insecure-Requests': '1',       # leads to an Error 503 on eBay
+            'DNT': '1', 
             'Cache-Control': 'no-cache',
             'Pragma': 'no-cache'
         }
-        response = requests.get(url, headers=headers)
-        if response.status_code != 200:
+        response = requests.get(url, headers=headers)   # send GET request to url
+        if response.status_code != 200:                 # check if the equest was successful
             print(f"Failed to fetch URL: {url} with status code: {response.status_code}")
             return response
 
-        soup = BeautifulSoup(response.content, 'html.parser')
-        review_sections = soup.find_all('div', {'class': 'ebay-review-section-r'})
+        soup = BeautifulSoup(response.content, 'html.parser')       # initialise parser object with bs4 
+        review_sections = soup.find_all('div', {'class': 'ebay-review-section-r'})  # find all review calsses
 
         reviews = []
-        for section in review_sections:
-            title = section.find('h3', {'class': 'review-item-title'})
-            review_text = section.find('p', {'class': 'review-item-content'})
-            if title and review_text:
-                reviews.append(f'{title.text.strip()}: {review_text.text.strip()}')
+        for section in review_sections:                                             # for each review div
+            title = section.find('h3', {'class': 'review-item-title'})              # get the title
+            review_text = section.find('p', {'class': 'review-item-content'})       # and the review body
+            if title and review_text:                                               # and if both are found
+                reviews.append(f'{title.text.strip()}: {review_text.text.strip()}') # append their text into a string
 
-        if soup.find(string="Sorry, no reviews match your current selections."):
-            raise IndexError("This page has no reviews.")
+        if soup.find(string="Sorry, no reviews match your current selections."):    # if there are no more reviews
+            raise IndexError("This page has no reviews.")                           # raise error because this is the last page
         
         return reviews
     
-    except requests.RequestException as e:
+    except requests.RequestException as e:                          # except all errers from the requests
         print(f"Error occurred while scraping {url}: {str(e)}")
         return []
 
